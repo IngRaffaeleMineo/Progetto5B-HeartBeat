ef50cc883f755adf43b92bfb9e1ea2f38df94dd0
Raffaele M <34072811+RaffaeleMineo@users.noreply.github.com>
2021-02-05 00:57:56 +0100

    Create Controller.java

---
+++ PingAckFaultDetector/src/main/java/com/dsbd2020/pingackfd/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.dsbd2020.pingackfd.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+
+@RestController
+public class Controller {
+    
+    @PostMapping("/ping")
+    @ResponseStatus(HttpStatus.OK)
+    public void Ping() {
+    }
+
+}
